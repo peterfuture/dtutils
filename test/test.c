0c8cf000b356ce51202d1921b517f8e849fcc52e
peter-s <peter_future@outlook.com>
2016-04-12 14:47:18 +0800

    init

---
+++ test/test.c
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *    Filename   :  test.c
+ *    Description:  
+ *    Version    :  1.0
+ *    Created    :  2016年04月12日 14时37分38秒
+ *    Revision   :  none
+ *    Compiler   :  gcc
+ *    Author     :  peter-s
+ *    Email      :  peter_future@outlook.com
+ *    Company    :  dt
+ *
+ * =====================================================================================
+ */
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
